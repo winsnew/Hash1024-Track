c3bb4f29b1f1549f8d58e1ce50d5e36123185188
n0t <lossany08@gmail.com>
2025-09-23 18:29:45 +0700

    add 32bit cuda

--- x32/x32.cu
+++ x32/x32.cu
@@ -1,1 +1,15 @@
-#include <keylockerintrin.h>
+#include <stdio.h>
+#include <stdint.h>
+
+__global__ void kernel32(uint32_t *a, uint32_t *b, uint32_t *add_result, 
+                        uint32_t *sub_result, uint32_t *mul_result, 
+                        uint32_t *div_result, int size) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    
+    if (idx < size) {
+        add_result[idx] = a[idx] + b[idx];
+        sub_result[idx] = a[idx] - b[idx];
+        mul_result[idx] = a[idx] * b[idx];
+        div_result[idx] = a[idx] / b[idx];
+    }
+}
