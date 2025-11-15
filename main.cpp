df4cd58bc8d8f1023f4dad1163c8ba5629e56836
n0t <lossany08@gmail.com>
2025-11-16 06:46:27 +0700

    fix sha256

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iomanip>
 #include <string>
 #include <vector>
-#include "sha256.cuh"
+#include "macro/sha256.cuh"
 
 void print_hash(const uint32_t* hash) {
     std::cout << std::hex << std::setfill('0');
